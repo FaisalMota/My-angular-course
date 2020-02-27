c66fe60491e4faee0e99572090ea45592bff29c1
Ayesha Mota <Ayesha.Mota@ssa.gov>
2020-02-27 13:50:07 -0500

    app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { AlbumListComponent } from "./albums/album-list/album-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/welcome", pathMatch: "full" },
+  { path: "welcome", component: WelcomeComponent },
+  { path: "albums", component: AlbumListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
